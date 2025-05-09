4dc3f854034af509b64475d164097b3bc26fac58
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:00:27 +0200

    add declarations and protected attributes for child classes

--- myCode/CANMessage.h
+++ myCode/CANMessage.h
@@ -5,11 +5,25 @@
  *      Author: mateo
  */
 
-#ifndef CANMESSAGE_H_
-#define CANMESSAGE_H_
+#ifndef CANMESSAGE_H
+#define CANMESSAGE_H
+
+#include <array>
+#include <stdint.h>
+#include <string>
 
 class CANMessage
 {
+protected:
+    uint8_t dlc; // data length code (0-8)
+    std::array<uint8_t, 8> data;
+
+public:
+    CANMessage(uint8_t dlc, const std::array<uint8_t, 8>& data);
+    virtual ~CANMessage() = default;
+
+    virtual uint32_t getId() const;
+    virtual std::string toString() const = 0;
 };
 
 #endif /* CANMESSAGE_H_ */
