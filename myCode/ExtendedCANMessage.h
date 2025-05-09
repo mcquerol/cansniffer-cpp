4d785f740d00f6940fbf474dee0cfb332585d8ba
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:10:29 +0200

    add declarations of methods and attributes

--- myCode/ExtendedCANMessage.h
+++ myCode/ExtendedCANMessage.h
@@ -8,8 +8,19 @@
 #ifndef EXTENDEDCANMESSAGE_H_
 #define EXTENDEDCANMESSAGE_H_
 
-class ExtendedCANMessage
+#include "CANMessage.h"
+
+class ExtendedCANMessage : public CANMessage
 {
+private:
+    uint32_t extendedID;  // 29-bit ID stored in 32-bit container
+
+public:
+    ExtendedCANMessage(uint32_t id, uint8_t dlc, const std::array<uint8_t, 8>& data);
+
+    uint32_t getID() const;
+    std::string toString() const;
 };
+
 
 #endif /* EXTENDEDCANMESSAGE_H_ */
