4987001247fa6006674b58975659a0bc9ee09058
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:33:38 +0200

    implement constructor, getter and toString

--- myCode/StandardCANMessage.cpp
+++ myCode/StandardCANMessage.cpp
@@ -7,3 +7,24 @@
 
 #include "StandardCANMessage.h"
 
+using namespace std;
+
+StandardCANMessage::StandardCANMessage(uint16_t id, uint8_t dlc, const std::array<uint8_t, 8> &data) : standardID{id}, CANMessage{dlc, data}
+{
+}
+
+uint32_t StandardCANMessage::getID() const
+{
+	return standardID;
+}
+
+std::string StandardCANMessage::toString() const
+{
+	string result;
+    result = "StandardID: 0x" + standardID + ", DLC: " + dlc << ", Data: ";
+    for (const auto& byte : getData())
+    {
+    	result += static_cast<int>(byte) << ' ';
+    }
+    return result;
+}
