40341cbcf229f4e9fdb1a5b9d1ca25fa2b609d8f
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:18:40 +0200

    add declarations for buus

--- myCode/CANBus.h
+++ myCode/CANBus.h
@@ -8,8 +8,21 @@
 #ifndef CANBUS_H_
 #define CANBUS_H_
 
-class VehicleCANBus
-{
+#include "CANMessage.h"
+#include <vector>
+#include <memory>
+
+class CANBus {
+private:
+    std::vector<std::shared_ptr<CANMessage>> messageQueue;
+
+public:
+    void sendMessage(const std::shared_ptr<CANMessage>& message);
+    std::shared_ptr<CANMessage> receiveMessage();
+    bool hasMessages() const;
+    void clear();
 };
+
+std::ostream operator<<(const std::ostream& out, const CANBus& bus);
 
 #endif /* CANBUS_H_ */
