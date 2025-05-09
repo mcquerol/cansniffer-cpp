f2cdb1fbed8dfe9985e4f65c59915726479bb14e
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:36:12 +0200

    implement constructor, getter and tostrting methods

--- myCode/ExtendedCANMessage.cpp
+++ myCode/ExtendedCANMessage.cpp
@@ -7,3 +7,24 @@
 
 #include "ExtendedCANMessage.h"
 
+using namespace std;
+
+ExtendedCANMessage::ExtendedCANMessage(uint32_t id, uint8_t dlc, const std::array<uint8_t, 8> &data) : extendedID{id}, CANMessage{dlc, data}
+{
+}
+
+uint32_t ExtendedCANMessage::getID() const
+{
+	return extendedID;
+}
+
+std::string ExtendedCANMessage::toString() const
+{
+	string result;
+    result = "ExtendedId: 0x" + extendedID + ", DLC: " + dlc << ", Data: ";
+    for (const auto& byte : getData())
+    {
+    	result += static_cast<int>(byte) << ' ';
+    }
+    return result;
+}
