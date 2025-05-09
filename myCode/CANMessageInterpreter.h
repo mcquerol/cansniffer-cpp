eb1010a4fe9e9cd1328674e2a24f42d7a9766d30
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:12:37 +0200

    add declarations of methods and attributes

--- myCode/CANMessageInterpreter.h
+++ myCode/CANMessageInterpreter.h
@@ -8,8 +8,15 @@
 #ifndef CANMESSAGEINTERPRETER_H_
 #define CANMESSAGEINTERPRETER_H_
 
+#include "CANMessage.h"
+#include <memory>
 
-
+class CANMessageInterpreter
+{
+public:
+    virtual ~CANMessageInterpreter() = default;
+    virtual void interpret(const std::shared_ptr<CANMessage>& message) = 0;
+};
 
 
 #endif /* CANMESSAGEINTERPRETER_H_ */
