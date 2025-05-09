93276e27f56b6349257703e0dd27c0491b9c7849
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:15:35 +0200

    added declarations

--- myCode/CANSniffer.h
+++ myCode/CANSniffer.h
@@ -8,8 +8,18 @@
 #ifndef CANSNIFFER_H_
 #define CANSNIFFER_H_
 
+#include "CANMessageInterpreter.h"
+#include <vector>
+#include <memory>
+
 class CANSniffer
 {
+private:
+    std::vector<std::shared_ptr<CANMessageInterpreter>> interpreters;
+
+public:
+    void addInterpreter(const std::shared_ptr<CANMessageInterpreter>& interpreter);
+    void receiveMessage(const std::shared_ptr<CANMessage>& message);
 };
 
 #endif /* CANSNIFFER_H_ */
