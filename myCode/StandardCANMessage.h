ee227f46f01cc9f3db42554f79d31ee41c65a94b
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-09 17:11:16 +0200

    add declarations of methods and attributes

--- myCode/StandardCANMessage.h
+++ myCode/StandardCANMessage.h
@@ -8,8 +8,18 @@
 #ifndef STANDARDCANMESSAGE_H_
 #define STANDARDCANMESSAGE_H_
 
-class StandardCANMessage
+#include "CANMessage.h"
+
+class StandardCANMessage : public CANMessage
 {
+private:
+    uint16_t standardID;  // 11-bit ID stored in 16-bit container
+
+public:
+    StandardCANMessage(uint16_t id, uint8_t dlc, const std::array<uint8_t, 8>& data);
+
+    uint32_t getID() const override;
+    std::string toString() const override;
 };
 
 #endif /* STANDARDCANMESSAGE_H_ */
