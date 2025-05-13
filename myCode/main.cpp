b0275e3c0647df22b7d39583c81f1306fb1642ca
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-05-13 05:07:47 +0200

    add sample test with two messages being added to the bus

--- myCode/main.cpp
+++ myCode/main.cpp
@@ -1,19 +1,27 @@
-// Standard (system) header files
 #include <iostream>
-#include <cstdlib>
-// Add more standard header files as required
-// #include <string>
+#include <array>
+#include <memory>
+#include "CANBus.h"
+#include "StandardCANMessage.h"
+#include "ExtendedCANMessage.h"
 
-using namespace std;
-
-// Add your project's header files here
-// #include "CFraction.h"
-
-// Main program
-int main ()
+int main()
 {
-    // TODO: Add your program code here
-	cout << "cansniffer started." << endl << endl;
+    CANBus bus;
 
-	return 0;
+    // Create some CAN messages
+    std::array<uint8_t, 8> data1 = {10, 20, 30, 40, 50, 60, 70, 80};
+    std::array<uint8_t, 8> data2 = {1, 2, 3, 4, 5, 6, 7, 8};
+
+    std::shared_ptr<CANMessage> msg1 = std::make_shared<StandardCANMessage>(0x123, 8, data1);
+    std::shared_ptr<CANMessage> msg2 = std::make_shared<ExtendedCANMessage>(0x1ABCDE, 8, data2);
+
+    // Send messages to the bus
+    bus.sendMessage(msg1);
+    bus.sendMessage(msg2);
+
+    // Print contents of the CAN bus
+    std::cout << "CAN Bus Contents:\n" << bus << std::endl;
+
+    return 0;
 }
